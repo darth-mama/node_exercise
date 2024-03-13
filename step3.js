ac9c3e1dbd9f4edcc5eb4d54bf76391371750ebe
darth-mama <128187728+darth-mama@users.noreply.github.com>
2024-03-13 18:41:05 -0500

    initial commit

---
+++ step3.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const process = require('process');
+const axios = require('axios');
+const {readFile, writeFile} = fs.promises;
+
+function cat(path) {
+  fs.readFile(path, 'utf8', function(err, data) {
+    if (err) {
+      console.error(`Error reading ${path}: ${err}`);
+      process.exit(1);
+    } else {
+      console.log(data);
+    }
+  });
+}
+
+cat(process.argv[2]);
+
+async function webCat(url) {
+    try {
+      let resp = await axios.get(url);
+      console.log(resp.data);
+    } catch (err) {
+      console.error(`Error fetching ${url}: ${err}`);
+      process.exit(1);
+    }
+  }
+  
+  let path = process.argv[2];
+  
+  if (path.slice(0, 4) === 'http') {
+    webCat(path);
+  } else {
+    cat(path);
+  }
+  
+  async function write(path, contents) {
+    try {
+        await writeFile(path, contents, "utf8");
+    } catch (err) {
+      console.error(`Error writing ${path}: ${err}`);
+      process.exit(1);
+    }
+}
+
+async function main() {
+    let path;
+    let out;
+  
+    if (process.argv[2] === '--out') {
+      out = process.argv[3];
+      path = process.argv[4];
+    } else {
+      path = process.argv[2];
+    }
+  
+    let contentsPromise = (path.slice(0, 4) === 'http')
+        ? webCat(path)
+        : cat(path);
+  
+    let contents = await contentsPromise;
+  
+    if (out) {
+        await write(out, contents);
+    } else {
+        console.log(contents);
+    }
+  }
+  
+  main();
